7fc3e9bfb92e45dc17e13dfce3451417cc5e5e5e
john mbugua <johnymbugua1997@gmail.com>
2023-12-18 17:03:01 +0300

    additional changes

--- src/component/tabButton.jsx
+++ src/component/tabButton.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 
-const TabButton = (props) =>{
-    return(
-         <>
-         {/* composition of the props children */}
-         {/*React will automatically pass the special prop children to every custom componenent */}
-          <button>{props.children}</button>
-         </>
-    )
-}
+const TabButton = ({ children, onSet, isSelected }) => {
+  return (
+    <>
+      {/* composition of the props children */}
+      {/*React will automatically pass the special prop children to every custom componenent */}
+      <button className={isSelected ? "active" : undefined} onClick={onSet}>
+        {children}
+      </button>
+    </>
+  );
+};
 
 export default TabButton;
